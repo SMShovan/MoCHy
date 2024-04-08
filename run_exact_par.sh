d0de5361c2bf3f17cf141cd4f5444d20af3c8883
sskg8 <sskg8@compute-12-37.mill>
2024-04-07 21:27:40 -0500

    Script updated

--- run_exact_par.sh
+++ run_exact_par.sh
@@ -1,3 +1,13 @@
 g++ -O3 -std=c++11 -lgomp -fopenmp main_exact_par.cpp -o exact_par;
-./exact_par 4;
+# Create or clear the file
+echo "Threads,Time (seconds)" > motif_counting_results.csv
+
+# Loop through the thread counts
+for i in 1 2 4 8 16 32 64; do
+    # Execute the program and capture the output
+    time=$(./exact_par $i | grep "Motif counting" | awk '{print $3}')
+    
+    # Write the thread count and time to the CSV file
+    echo "$i,$time" >> motif_counting_results.csv
+done
 rm exact_par;
